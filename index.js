364f674ba0e324e9d904bef1575ea51f1c94e1e1
Martha <martha@uber.com>
2013-09-15 15:14:30 +0200

    listening on correct port

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.get('/:twitterHandle', function (req, res) {
   res.send('Hello world, ' + twitterHandle);
 });
 
-if ('development' == app.get('env')) {
-  app.listen(3000);
-  console.log('Listening on port 3000');
-}
+app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
