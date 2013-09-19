e874181a690e7cd86e643b139146342ccaa07a91
Martha <martha@uber.com>
2013-09-19 16:32:19 -0500

    real favicon

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var express = require('express'),
 
 var app = express();
 
+app.use(express.favicon("favicon.ico")); 
+
 if (process.env.NODE_ENV === "development") {
   var config = require('./config.json');
 
